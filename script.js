b014606aa9a68ff6bc529cf1020aa66ddac6535f
西脇宏太郎 <hanabi0703@gmail.com>
2020-04-05 22:45:42 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// $('.mv_image').slick();
+
+// $(function(){
+//   $('.mv_image').each(function(){
+//     var sildes = $(this).find('img'),
+//         slideCount = slides.length,
+//         currentIndex = 0;
+//
+//         slides.eq(currentIndex).fadeIn();
+//
+//         setInterval(showNextSlide, 7500);
+//
+//         function showNextSlide () {
+//           var nextIndex = (currentIndex + 1) % slideCount;
+//
+//           slides.eq(currentIndex).fadeOut();
+//           slides.eq(nextIndex).fadeIn();
+//           currentIndex = nextIndex;
+//         }
+//   });
+// });
